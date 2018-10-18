70fb96b91ee5051509349650519bab84b1ea1eb5
ed.lopez1905@gmail.com <ed.lopez1905@gmail.com>
2018-10-17 17:49:56 -0700

    upload

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Hello, please enter a number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Please enter an operator: ");
+            string oP = Console.ReadLine();
+
+            Console.Write("Hello, please a second number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+
+            if (oP == "+")
+            {
+                Console.WriteLine(num1 + num2);
+            }
+            else if (oP == "-")
+            {
+                Console.WriteLine(num1 - num2);
+            }
+            else if (oP == "/")
+            {
+                Console.WriteLine(num1 / num2);
+            }
+            else if (oP == "*")
+            {
+                Console.WriteLine(num1 * num2);
+            }
+            else
+            {
+                Console.WriteLine("Invalid input");
+            }
+
+
+
+
+
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
